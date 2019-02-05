1e8a395a15e11b55ffbbbd1b19cb09cb2dbc8e59
alumno <alumno>
2019-02-04 22:12:09 -0300

    tarea

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	//https://www.w3schools.com/jsref/met_win_prompt.asp  informacion del promt
+	var nombre = prompt("Porfavor ingrese su nombre");  //las variables lo mas explisitas posible
+    alert("Nombre ingresado "+nombre);  //puse un + de mas en el promt y bugeo todo el ejecutable
+    var b = "Su nombre ah sido cargado exitosamente "
+    alert(b);
 }
 
