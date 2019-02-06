56f7328679831c3d39ea564737fd2a2b8e5eeb34
alumno <alumno>
2019-02-06 19:54:54 -0300

    hice el 5

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -1,7 +1,13 @@
 /*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
-{	
-	
-}
-
+{	//ingreso variables
+	var nombre;
+	var edad;
+	//le doy  valora las variables
+	 nombre = elNombre.value;
+	 edad = laEdad.value;
+	 //ejecuto la variable
+ // alert(nombre + edad); ejemplo anterior
+ alert("Usted se llama "+ nombre +" y tiene "+ edad + "años");
+} 
