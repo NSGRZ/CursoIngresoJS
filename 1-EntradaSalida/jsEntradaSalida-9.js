5468bcfcb987377d9d8a42703638962ff76df983
alumno <alumno>
2019-02-06 22:12:48 -0300

    intento de 9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var Valor;
+    var Resultad;
+
+Valor = sueldo.value;
+Valor = parseInt(Valor);
+Resultad = resultado.value;
+Resultad = Valor*1.1;
 
 	
 }
