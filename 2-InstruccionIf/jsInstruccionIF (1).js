bb96b4ce1e405f05f509be768b005deff10a9b7d
alumno <alumno>
2019-02-11 19:22:20 -0300

    el if lindo

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -4,8 +4,10 @@ Numero = edad.value;
 Numero = parseInt(Numero);
 //tomo la niña
   
-if(Numero === 15){ //la llave va arriba para decirle a la maquina que la sentencia es del if
-alert("niña bonita");
-}
+if(Numero == 15) //la llave va arriba para decirle a la maquina que la sentencia es del if
+{alert("niña bonita");}
+else //sin condicion porque ya està en el if
+{alert("Edad  no reconocida");}
+
 
 }//FIN DE LA FUNCIÓN
