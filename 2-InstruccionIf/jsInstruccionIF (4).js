6ce7bc55e9a44f1a56574599604242efe51ee998
alumno <alumno>
2019-02-11 20:20:19 -0300

    la forma con &&

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+var Numero;
+Numero = edad.value;
+Numero = parseInt(Numero);
+  
+/*if(Numero >12){
+	if(Numero <18)
+	{ alert("adolescente")}
 
-
-
+}
+*/
+if(Numero>12&&Numero<18)
+{ alert("adolescente")}
 }//FIN DE LA FUNCIÓN
