8adfd43e1f88652cdad588d1c430709a3dcbbdca
alumno <alumno>
2019-02-11 20:40:54 -0300

    formas de resolver el 5

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,15 @@
 function mostrar()
-{
+{var Numero;
+	Numero = edad.value;
+	Numero = parseInt(Numero);
 //tomo la edad  
+/*if(Numero > 12 && Numero < 17)
+ {console.log("son adolescentes")}
+else
+	{alert("NO ES adolescente")}*/
 
-
+/*if(!(Numero > 12 && Numero <= 17))
+alert("no es adolescente")*/ //de esta forma se aplica distrivutiba al if 
+if(Numero <13 || Numero>17) //la forma O para resolver
+alert("no es adolescente")
 }//FIN DE LA FUNCIÓN
