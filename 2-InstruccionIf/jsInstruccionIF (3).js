f77efecafffa12812cdbf4c5cf65181e7d9b5c51
alumno <alumno>
2019-02-11 20:02:09 -0300

    if else

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -9,8 +9,10 @@ Numero = parseInt(Numero);//parsear sirve para comprar numeros,pero podemos comp
 //tomo la niña
   
 if(Numero >17) //la llave va arriba para decirle a la maquina que la sentencia es del if
-{alert("sos mayor de edad");}
-if (Numero<18) //sin condicion porque ya està en el if
-{alert("sos menor de edad");}
+{alert("Sos mayor de edad");}
+if (Numero<18) 
+{alert("Sos menor de edad");}
+/* else - otra manera de hacer la accion,pero te si escribis "wsad" seria lo contrario a if,y te lo tomaria el alert 
+{alert("Sos menor de edad");}*/
 
 }//FIN DE LA FUNCIÓN
