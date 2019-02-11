1198eefb7c008e0011d435a424b88eb5d757238b
alumno <alumno>
2019-02-11 20:21:27 -0300

    comentarios para el 4

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -8,9 +8,9 @@ Numero = parseInt(Numero);
 /*if(Numero >12){
 	if(Numero <18)
 	{ alert("adolescente")}
-
-}
+esta forma  sirve para hacer otras cosas con Else
+} el if dentro del if  
 */
-if(Numero>12&&Numero<18)
+if(Numero>12&&Numero<18) //ahorra espacio pero no podemos manipular dentro de 12 y 18
 { alert("adolescente")}
 }//FIN DE LA FUNCIÓN
