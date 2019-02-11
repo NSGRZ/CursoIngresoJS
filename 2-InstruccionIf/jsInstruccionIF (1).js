0b533527f9d239e7bd3205f225103c1e41b4a8b0
alumno <alumno>
2019-02-11 19:02:19 -0300

    el if correcto es ==

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -4,7 +4,7 @@ Numero = edad.value;
 Numero = parseInt(Numero);
 //tomo la niña
   
-if(Numero >= 15){ //la llave va arriba para decirle a la maquina que la sentencia es del if
+if(Numero === 15){ //la llave va arriba para decirle a la maquina que la sentencia es del if
 alert("niña bonita");
 }
 
