6b77a0cf5c90e4584499ed3c0483dcc771e50181
alumno <alumno>
2019-02-11 21:29:14 -0300

    if else if else

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,21 @@
 function mostrar()
-{
-//tomo la edad  
+{var Numero;
+	 Numero = edad.value;
+	 Numero = parseInt(Numero);
+if(Numero>17) //si es mayor de 17
+	{alert("usted es mayor de edad")}
+
+else // si no es mayor a 17...
+	if(Numero>12) //es mayor a 12?
+		{alert("adolescente")}
+	else //si no es un niño
+		{alert("niño")} //niño
+	
 
 
 
 
-}//FIN DE LA FUNCIÓN
+
+
+
+}
