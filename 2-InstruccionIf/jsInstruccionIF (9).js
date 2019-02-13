cb8cba4a11e738374919c74d78df4899be180035
NSGRZ <sgr.nicolas@yahoo.com>
2019-02-13 13:59:25 -0300

    el 9

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	var Numerito; //ingreso la variable
+	var contador
+	Numerito = Math.floor((Math.random() * 10) + 1); //la variable tiene propiedad Aleatoria del  1 al 10
+	Numerito = parseInt(Numerito);
+	
+    alert(Numerito); //muestro la variable con el numero
+    
 	
 
-}//FIN DE LA FUNCIÓN
+}
