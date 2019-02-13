31a2bca3a58f4e6d78e68a2246545d7158766073
NSGRZ <sgr.nicolas@yahoo.com>
2019-02-13 14:05:57 -0300

    recuperatorio

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,15 @@
 
 function mostrar()
 {
+alert("Busquemos el perimetro de un rectangulo");
+var ancho;
+var largo;
+var resultado;
 
+ ancho = prompt("Ingrese el ancho en metros");
+ largo = prompt("Ingrese el largo en metros");
+ ancho = parseInt(ancho);
+ largo = parseInt(largo);
+ resultado = ancho*largo;
+ alert("el perimetro es :"+resultado);
 }
