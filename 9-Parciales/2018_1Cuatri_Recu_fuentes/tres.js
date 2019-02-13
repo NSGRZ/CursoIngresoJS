705ff0a13c8f643d3e73d470d6c2cf506b155eb1
NSGRZ <sgr.nicolas@yahoo.com>
2019-02-13 14:11:02 -0300

    modifique el recuperatorio 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -4,15 +4,15 @@ var procentaje;
 var monto;
 var resultado;
 //ungreso variables
-monto = prompt("ingrese el monto a descontar");
-procentaje = prompt("ingrese el descuento");
+monto = prompt("ingrese el monto a aumento");
+procentaje = prompt("ingrese el aumento");
 //el usuario le da un valor
 procentaje = parseInt(procentaje);
 monto = parseInt(monto);
 //el valor ingresado lo transformo en entero
 resultado = monto*procentaje/100;
-//alert("el monto descontado fue de :"+resultado);
-resultado = monto-resultado;
+alert("el monto aumentado fue de :"+resultado);
+resultado = monto+resultado;
 //realizo una ecuacion con los valores 
 elPrecioFinal.value = resultado; 
 //lo muestro por ID
