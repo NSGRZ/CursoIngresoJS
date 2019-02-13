c9f1936fbf7ff0977ec54ae87ccec71c3d5b5ebf
alumno <alumno>
2019-02-13 20:11:40 -0300

    el 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -3,24 +3,44 @@ function mostrar()
 var productoUno;
 var productoDos;
 var SumaProductos;
-var DescuentoUno;
-var DescuentoDos;
+var Descuento;
+var Pago;
 var Resultado
 var Recargo;
+alert("ingrese tu tipo de pago")
+alert("si es con TARJETA ingrese 1")
+alert("si es EFECTIVO ingrese 2")
+pago = prompt("ingrese su tu tipo de pago")
 productoUno = prompt("ingrese el valor del producto uno");
 productoDos = prompt("ingrese el valor del producto dos"); // ingreso valores
-descuento = 1.1
+descuento = 10
 productoUno = parseInt(productoUno);
 productoDos = parseInt(productoDos);
 
 SumaProductos = productoDos+productoUno;
-Resultado = SumaProductos * descuento
-
-alert("la suma del producto es "+SumaProductos)
-alert("la suma de los productos con el descuento del 10% es: " +Resultado);
+Resultado = (SumaProductos *descuento)/100
+Resultado =    SumaProductos - Resultado
 
 
-//if()
+
+
+if(SumaProductos >2000)
+	{Resultado = SumaProductos*15/100
+	Resultado =  SumaProductos-Resultado 
+  alert("Tenes un descuento adicional por superar el monto de $2000 del 15%, Tu Nuevo precio final es"+Resultado)}
+  else
+  	if(pago == 1)
+     { 
+     	Resultado = (SumaProductos *descuento)/100
+  		Resultado =  Resultado + SumaProductos
+  	  }
+     else 
+
+      {alert("la suma del producto es "+SumaProductos)
+       alert("la suma de los productos con el descuento del 10% es: " +Resultado);}
+
+
+
 }
 
 
