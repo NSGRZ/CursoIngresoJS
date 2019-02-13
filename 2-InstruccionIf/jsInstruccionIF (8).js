e63ce977cedfb1a0e1f61af33794707b4c7f5d60
NSGRZ <sgr.nicolas@yahoo.com>
2019-02-13 13:59:00 -0300

    el 8 en casa

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,26 @@
 function mostrar()
 {
-//tomo la edad  
-
+	var EdadDe 
+	EdadDe=edad.value 
+	EdadDe=parseInt(EdadDe) 
+	if(EdadDe <18 && estadoCivil.value == "Soltero") //si es menor de 18 y el estado civil es igual a soltero
+		{
+		console.log("nada")  //no hago nada
+		}
+		else //pero 
+			{if(EdadDe > 17 && estadoCivil.value == "Soltero") //si es mayor de 18 y el estado civil es igual a solter
+						{
+							alert("Es "+estadoCivil.value+ " y no es menor.") //digo el estado civil y que no es menor
+						}
+			}
 	
 
 
-}//FIN DE LA FUNCIÓN
+}
+
+
+
+/*si es < 18 - "soltero"
+"nada"
+si es != a < 18
+"estadoCivil.value" y no es menor */
