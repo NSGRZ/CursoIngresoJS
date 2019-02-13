b2c2247adec01dce91b2a34aa85db14f2079c6a8
NSGRZ <sgr.nicolas@yahoo.com>
2019-02-13 13:59:51 -0300

    el 10 en casa

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	var Numerito; //ingreso la variable
+	var contador
+	Numerito = Math.floor((Math.random() * 10) + 1); //la variable tiene propiedad Aleatoria del  1 al 10
+	Numerito = parseInt(Numerito);
 	
-
+    alert(Numerito); 
+	if (Numerito >8)
+		alert("EXCELENTE");
+	{else
+			{if(Numerito >3)
+							alert("APROBÓ");}
+			else
+				{if(Numerito <4)
+									alert("Vamos, la proxima se puede" );}
+	}
 }//FIN DE LA FUNCIÓN
