ee7e91b8f90ec7e3686dc1857a89f42a67be0efb
alumno <alumno>
2019-02-13 19:48:43 -0300

    ejersicio 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,30 @@
 function mostrar()
 {
+var productoUno;
+var productoDos;
+var SumaProductos;
+var DescuentoUno;
+var DescuentoDos;
+var Resultado
+var Recargo;
+productoUno = prompt("ingrese el valor del producto uno");
+productoDos = prompt("ingrese el valor del producto dos"); // ingreso valores
+descuento = 1.1
+productoUno = parseInt(productoUno);
+productoDos = parseInt(productoDos);
 
+SumaProductos = productoDos+productoUno;
+Resultado = SumaProductos * descuento
+
+alert("la suma del producto es "+SumaProductos)
+alert("la suma de los productos con el descuento del 10% es: " +Resultado);
+
+
+//if()
 }
+
+
+//al realizar una compra si compra de mas de dos productos se realiza un descuento de 10% y si supera los 2000 
+//se agrega un descuento adicional del 15% 
+
+//si el pago es con tarjeta y no efectivo al pprecio final se le agrega unm 10 % de recargo
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,25 +1,26 @@
 function mostrar()
 {
-var procentaje;
-var monto;
-var resultado;
-var resultadoA
-var Nombre
+	var procentaje;
+	var monto;
+	var resultado;
+	var resultadoA
+	var Nombre
 
-Nombre = elNombre.value
+	Nombre = elNombre.value
 
-procentaje = 1;
-monto = laLocalidad.value;
+	procentaje = 10//listo
+	monto = laLocalidad.value;
 
 
-monto = parseInt(monto);
+	monto = parseInt(monto);
 
-resultadoA = (monto*procentaje)/100;
+	resultadoA = (monto*procentaje)/100;
 
-resultado = monto+resultadoA;
-
- 
-alert("hola "+Nombre+" El precio del producto en efectivo es$ "+monto+" con tarjeta tiene un recargo del 10% que seria "+resultadoA+" El precio final es de "+resultado)
+	resultado = monto+resultadoA;
+	// si sale 200 el 10% es 20 y el final es  220
+	 
+	
+	alert("hola "+Nombre+" El precio del producto en efectivo es$ "+monto+" con tarjeta tiene un recargo del 10% que seria "+resultadoA+" El precio final es de "+resultado)
 }
 //alert("el precio del producto en efectivo es $XXXXXX con 
 //tarjeta tiene un recargo del 10% que seria $XX de recargo ,costandole final $XXX)
