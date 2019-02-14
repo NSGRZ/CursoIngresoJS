2480f72bdf8b33d5ede44d3fe59a950d7b9e8a49
alumno <alumno>
2019-02-13 21:06:41 -0300

    el ejersicio 4 del parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -7,10 +7,12 @@ var Descuento;
 var Pago;
 var Resultado
 var Recargo;
+var Resultadocondiez;
 alert("ingrese tu tipo de pago")
 alert("si es con TARJETA ingrese 1")
 alert("si es EFECTIVO ingrese 2")
 pago = prompt("ingrese su tu tipo de pago")
+pago = parseInt(pago)
 productoUno = prompt("ingrese el valor del producto uno");
 productoDos = prompt("ingrese el valor del producto dos"); // ingreso valores
 descuento = 10
@@ -18,27 +20,39 @@ productoUno = parseInt(productoUno);
 productoDos = parseInt(productoDos);
 
 SumaProductos = productoDos+productoUno;
-Resultado = (SumaProductos *descuento)/100
-Resultado =    SumaProductos - Resultado
 
 
+if(SumaProductos >=2000 && pago == 1)
 
+	{
+	
+	Resultado = Resultado-SumaProductos
+	Resultado = SumaProductos*15/100
+	Resultado = SumaProductos-Resultado
+	
 
-if(SumaProductos >2000)
-	{Resultado = SumaProductos*15/100
-	Resultado =  SumaProductos-Resultado 
-  alert("Tenes un descuento adicional por superar el monto de $2000 del 15%, Tu Nuevo precio final es"+Resultado)}
-  else
-  	if(pago == 1)
-     { 
-     	Resultado = (SumaProductos *descuento)/100
-  		Resultado =  Resultado + SumaProductos
-  	  }
-     else 
-
-      {alert("la suma del producto es "+SumaProductos)
-       alert("la suma de los productos con el descuento del 10% es: " +Resultado);}
-
+  alert("Por comprar dos productos tenes un descuento del 10%, ademas tenes un descuento adicional por superar el monto de $2000 del 15% ,pero al pagar con tarjeta tenes un recargo del 10%, Tu Nuevo precio final es"+Resultado)}
+  
+ if(pago == 2 && SumaProductos>2000 )
+     {
+     	
+    Resultado = SumaProductos*1.1
+	Resultado = Resultado-SumaProductos
+	Resultado = SumaProductos*15/100
+	Resultado = SumaProductos-Resultado
+	
+  	alert("Tenes un descuento por comprar dos productos del 10%,ademas  por superar el monto de $2000 del 15% tu precio final es de :"+Resultado)
+  }
+   if (pago == 1 && SumaProductos<2000)  
+     {
+      alert("Tenes un descuento por comprar dos productos del 10%,pero al pagar con tarjeta tenes un recargo del 10%, Tu Nuevo precio final es"+SumaProductos)
+      }
+	if (pago == 2&&SumaProductos<2000) 
+{
+  	Resultado = SumaProductos*10/100
+	Resultado = SumaProductos-Resultado
+	alert("Tenes un descuento por comprar dos productos del 10% tu precio final es de :"+Resultado)
+}
 
 
 }
