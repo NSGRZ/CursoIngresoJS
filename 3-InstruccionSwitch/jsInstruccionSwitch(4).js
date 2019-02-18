264b2475825435d929838bd4319758f3e10908f6
alumno <alumno>
2019-02-18 20:32:06 -0300

    switch4

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -2,11 +2,35 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+var mensaje=""
+switch(mesDelAño)
+{
+	case "Enero":
+ 	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	case "Marzo":
+	case "Mayo":
+	case "Julio":
+	case "Agosto":
+	mensaje="este mes tiene 31 dias"
+	break;
 
-//alert (mesDelAño);
+	case "Febrero":
+	mensaje="este mes tiene 28 y cada tanto 29,pero nunca 30  dias"
+	break;
+		case "Septiembre":
+	 	case "Abril":
+		case "Junio":
+		mensaje="este mes tiene 30 dias"
+		break;
+
+	}
+	if(mensaje!="")
+	{alert(mensaje)}
 	
 	
 
 
 
-}//FIN DE LA FUNCIÓN
+}
