964305116ea147e8a2bcd35b3b87174d74c7e2f4
alumno <alumno>
2019-02-18 20:22:09 -0300

    punto2switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,9 +3,35 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+	case "Enero":
+	
+	case "Febrero":
+	
+	case "Marzo":
+	 
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	mensaje="Falta para el invierno." 
+	 break;
+	case "Julio":
+	case "Agosto":
+	 mensaje="Abrigate que hace frio."
+	 break;
+
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	mensaje="Ya pasamos el frio, ahora calor!!!."
+	break;
+
+	}
+	if(mensaje!="")
+	{alert(mensaje)}
+
+}
 
 
-
-
-}//FIN DE LA FUNCIÓN
