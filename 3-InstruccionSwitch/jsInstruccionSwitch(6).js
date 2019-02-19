2cff1048e239c9ebd952720a556481c9b55e7042
alumno <alumno>
2019-02-18 21:54:05 -0300

    elwhile

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -3,6 +3,42 @@ function mostrar()
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
 
-
+laHora=parseInt(laHora)
+switch(laHora)
+{
+	case 1:
+ 	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	mensaje="es re de mañana volve a dormir"
+	break;
+	case 7:
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	mensaje="Es de mañana"
+	break;
+	case 12:
+	mensaje="es el mediodia hora de almorzar"
+	break;
+	case 13:
+	case 14:
+	case 15:
+	case 16:
+	case 17:
+	case 18:
+	case 19:
+	case 20:
+	case 21:
+	case 22:
+	case 23:
+	case 00:
+	}
+	if(mensaje!="")
+	{alert(mensaje)}
+	
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,14 @@
 function mostrar()
 {
-	alert('iteración while');
-
+	var contador
+	contador=0
+	while(contador<10)
+	{ 
+		//console.log(contador); aqui muestra el producto mientras se trabaja el while pero con una interaccion menos
+		contador++
+		//alert(contador)
+		console.log(contador);//console o alert dentro de el while para ver como evoluciona 
+	}
+	alert(contador)//te muestra el producto final del el while
 
 }//FIN DE LA FUNCIÓN
