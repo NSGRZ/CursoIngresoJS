8e432b1f08311d1de2c4bc31391e8b803164927a
NSGRZ <sgr.nicolas@yahoo.com>
2019-02-20 12:59:16 -0300

    funcionamientodel4

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,13 +1,20 @@
 function mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
-	var Validacion
-	numero = parseInt(numero)
-	Validacion = Numero.value
-		while(numero <10)
-		{numero=Numero.value
-			numero++}
 	
+	var numero =prompt("ingrese un número entre 0 y 10.");
+	
+	
+		while(numero >9)
+		{ 	numero = prompt("ingrese un número entre 0 y 9.");
+			numero = parseInt(numero)
+			if(numero <=9)
+			{
+				
+				break;
+			}
+
+			}
+		Numero.value=numero
 }//FIN DE LA FUNCIÓN
 //al presionar el botón pedir un número entre 0 y 9 inclusive.
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,8 +3,12 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-
-
+while(true) //mientras 
+{	contador +1
+	contador =	acumulador
+	if(contador == 1)
+	{break;}
+}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
