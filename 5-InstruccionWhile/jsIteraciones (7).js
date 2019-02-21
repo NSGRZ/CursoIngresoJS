8023ad388c69c2ef2b73a1177a64574e9d719c46
alumno <alumno>
2019-02-20 22:05:57 -0300

    while7

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,7 +4,15 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero
+while(respuesta== "si")
+{ numero =prompt("ingrese un numero")
+	numero =parseInt(numero)
+	contador=contador+1
+	acumulador=numero+acumulador
+respuesta=prompt("desea continuar?")
 
+}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
