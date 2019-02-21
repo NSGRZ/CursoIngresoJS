687957ff3efeb0fe8dcb4a237ca0b3395a54bd17
alumno <alumno>
2019-02-20 22:15:10 -0300

    intentowhile8

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,9 +4,19 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
-	var respuesta='si';
+	var Numero
+	var respuesta="si";
+while(respuesta== "si")
+{ Numero =prompt("ingrese un numero")
+	Numero =parseInt(Numero)
+	if (numero<0) {
+		negativo=negativo*Numero
+	}
+	else
+		{positivo=positovo+Numero}
+respuesta=prompt("desea continuar?")
 
+}
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
